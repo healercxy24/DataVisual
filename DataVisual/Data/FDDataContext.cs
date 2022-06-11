48195414c99a11df938757298b27352ca9307f9b
cxy-thinkpad <njucxy0824@gmail.com>
2022-06-11 19:41:14 +0200

    add efcore and dbcontext.cs.

---
+++ DataVisual/Data/FDDataContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataVisual.Data
+{
+    public class FDDataContext : DbContext
+    {
+        public FDDataContext(DbContextOptions<FDDataContext> options) : base(options) { }
+        public DbSet<FD1RUL> FD1RULs { get; set; }
+    }
+}
