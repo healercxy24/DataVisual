6394f2ac5bf7ba87c747e674217b1b850efe8358
cxy-thinkpad <njucxy0824@gmail.com>
2022-06-12 15:18:32 +0200

    add service and fd001.razor -> work well.

--- DataVisual/Data/FD001Service.cs
+++ DataVisual/Data/FD001Service.cs
@@ -1,8 +1,28 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+using System.Linq;
 
 namespace DataVisual.Data
 {
     public class FD001Service
     {
+        private readonly FDDataContext _context;
+
+        public FD001Service(FDDataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<FD1RUL>> GetFD1RUL()
+        {
+            return await _context.FD1RULs.ToListAsync();
+        }
+
+        public async Task<FD1RUL> GetSingleFD1RUL(int Id)
+        {
+            return await _context.FD1RULs.FirstOrDefaultAsync(h => h.EngineID == Id);
+        }
     }
 }
